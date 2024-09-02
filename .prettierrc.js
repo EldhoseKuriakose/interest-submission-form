6d18dd618157f83525f1637d4f86738e37f14364
EldhoseKuriakose <eldhosekuriakose96@gmail.com>
2024-09-02 22:06:17 +0530

    installed necessary packages

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'always',
+  tabWidth: 2,
+  printWidth: 100,
+  semi: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  quoteProps: 'as-needed',
+  endOfLine: 'auto'
+};
